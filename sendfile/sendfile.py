1f283deb8b81e593a816ef7271c34cbe9956386c
JakesDen <jakesden11@gmail.com>
2016-11-16 23:30:08 +0000

    Update sendfile.py

--- sendfile/sendfile.py
+++ sendfile/sendfile.py
@@ -1,20 +1,17 @@
-import logging
-import logging.handlers
-import asyncio
-import discord
-from cogs.utils.settings import Settings
-from cogs.utils.dataIO import dataIO
 from .utils import checks
 from discord.ext import commands
 
 class sendfilecog:
-    """Sends files using a command. to send cogs like sendfile do [prefix]sendfile cogs/sendfile.py {optional message}"""
+    """Sends files using a command.
+    to send cogs like sendfile do:
+    [prefix]sendfile cogs/sendfile.py {optional message}"""
 
     def __init__(self, bot):
         self.bot = bot
-        
+        self.aliases = ["sf", "sendf", "filesend", "fs"]
+
     @checks.is_owner()
-    @commands.command(aliases=["sf", "sendf", "filesend", "fs"], pass_context=True, description="Usage: [prefix]sendfile [file] {optional message}")
+    @commands.command(aliases=delf.aliases, pass_context=True, description="Usage: [prefix]sendfile [file] {optional message}")
     async def sendfile(self, ctx, file, optionalmessage=""):
         with open(file, 'rb') as j:
             await self.bot.send_file(ctx.message.channel, fp=j, content=optionalmessage)
