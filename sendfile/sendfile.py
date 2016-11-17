5949e2699aa80f87ac57aa99f97b3bf558c1a079
JakesDen <jakesden11@gmail.com>
2016-11-17 00:09:03 +0000

    Update sendfile.py

--- sendfile/sendfile.py
+++ sendfile/sendfile.py
@@ -1,6 +1,7 @@
 from .utils import checks
 from discord.ext import commands
 
+
 class sendfilecog:
     """Sends files using a command.
     to send cogs like sendfile do:
@@ -9,9 +10,10 @@ class sendfilecog:
     def __init__(self, bot):
         self.bot = bot
         self.aliases = ["sf", "sendf", "filesend", "fs"]
+        self.help = "Usage: [prefix]sendfile [file] {optional message}"
 
     @checks.is_owner()
-    @commands.command(aliases=delf.aliases, pass_context=True, description="Usage: [prefix]sendfile [file] {optional message}")
+    @commands.command(aliases=self.aliases, pass_context=True, description=self.help)
     async def sendfile(self, ctx, file, optionalmessage=""):
         with open(file, 'rb') as j:
             await self.bot.send_file(ctx.message.channel, fp=j, content=optionalmessage)
